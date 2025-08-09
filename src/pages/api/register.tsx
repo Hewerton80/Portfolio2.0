db0dc26bf479fe5fa5b01480e25c9d8d5827a01b
hewerton <hewerton80@gmail.com>
2025-08-09 14:28:58 -0300

    remove prisma

--- src/pages/api/register.tsx
+++ src/pages/api/register.tsx
@@ -1,20 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '../../../lib/primsa'
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// // import prisma from '../../../lib/primsa'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  const { register: myIp } = req.body
-  try {
-    const now = new Date()
-    const timezoneOffset = now.getTimezoneOffset()
-    const timezoneOffsetHours = timezoneOffset / 60
-    now.setHours(now.getHours() - timezoneOffsetHours)
-    const ip = await prisma.ip.create({
-      data: { ip: myIp, createdAt: now.toISOString() },
-    })
-    return res.status(201).json({ ip })
-  } catch (error) {
-    console.log('erro', error)
-    return res.status(400).json({ success: false })
-  }
-}
+// export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+//   const { register: myIp } = req.body
+//   try {
+//     const now = new Date()
+//     const timezoneOffset = now.getTimezoneOffset()
+//     const timezoneOffsetHours = timezoneOffset / 60
+//     now.setHours(now.getHours() - timezoneOffsetHours)
+//     const ip = await prisma.ip.create({
+//       data: { ip: myIp, createdAt: now.toISOString() },
+//     })
+//     return res.status(201).json({ ip })
+//   } catch (error) {
+//     // console.log('erro', error)
+//     // return res.status(400).json({ success: false })
+//   }
+// }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,28 +8,28 @@ import HomeSection from '../components/pageSections/HomeSection'
 import MyPortfolioSection from '../components/pageSections/MyPortfolioSection'
 
 const Home: NextPage = () => {
-  const saveIpAdress = useCallback(async () => {
-    let myIp = ''
-    try {
-      const { data: idObject } = await axios.get('https://api.ipify.org/?format=json')
-      myIp = idObject.ip
-    } catch (err) {
-      console.log('register erro')
-    }
-    try {
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/register`,
-        { register: myIp }
-      )
-      console.log('response:', response.data)
-    } catch (error) {
-      console.log('error:', error)
-    }
-  }, [])
+  // const saveIpAdress = useCallback(async () => {
+  //   let myIp = ''
+  //   try {
+  //     const { data: idObject } = await axios.get('https://api.ipify.org/?format=json')
+  //     myIp = idObject.ip
+  //   } catch (err) {
+  //     console.log('register erro')
+  //   }
+  //   try {
+  //     const response = await axios.post(
+  //       `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/register`,
+  //       { register: myIp }
+  //     )
+  //     console.log('response:', response.data)
+  //   } catch (error) {
+  //     console.log('error:', error)
+  //   }
+  // }, [])
 
-  useEffect(() => {
-    saveIpAdress()
-  }, [saveIpAdress])
+  // useEffect(() => {
+  //   saveIpAdress()
+  // }, [saveIpAdress])
 
   return (
     <>
